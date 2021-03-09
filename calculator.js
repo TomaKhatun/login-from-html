e65811d711bafcce56cdf27a5f18abb60d194030
TomaKhatun <khatuntoma94@gmail.com>
2021-03-09 18:38:48 +0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function farenheightToCelsius(farenheight){
+    var Celsius =(farenheight-32)/1.8;
+    return Celsius;
+}
+function cnTm(cn){
+    var m=(cn/100);
+    return m;
+}
+function meterTfeet(feet){
+    var meter=(feet/3.2888);
+    return meter;
+}
